e84fb9a5da46d5c7f48fecd32c261fa69c17f51e
ferid-kerimli <faridkaarimli@gmail.com>
2025-01-25 11:50:25 +0400

    Commit

--- week1/LcmOfTwoIntegers.cpp
+++ week1/LcmOfTwoIntegers.cpp
@@ -10,9 +10,13 @@ int gcd(int a, int b) {
     return gcd(b, a % b);
 }
 
+long long lcm(int a, int b) {
+    return (long long)a * (b / gcd(a, b));
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << gcd(a, b) << endl;
+    cout << lcm(a, b) << endl;
     return 0;
 }
