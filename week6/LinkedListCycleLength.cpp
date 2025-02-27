49bd59eafdbf1e87ba5f13213c0308afb44feeff
ferid-kerimli <faridkaarimli@gmail.com>
2025-02-27 20:50:59 +0400

    commit

--- week6/LinkedListCycleLength.cpp
+++ week6/LinkedListCycleLength.cpp
@@ -9,44 +9,27 @@ public:
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 
-    ListNode *detectCycle(ListNode *head)
-    {
-        if (!head || !head->next)
-            return NULL;
-
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        while (fast && fast->next)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if (slow == fast)
-            {
-                return slow;
+    int CycleLength(ListNode* head) {
+        ListNode* a = head;
+        ListNode* b = head;
+    
+        while (b && b->next) {
+            a = a->next;
+            b = b->next->next;
+    
+            if (a == b) {
+                int length = 1;
+                ListNode* current = a->next;
+    
+                while (current != a) {
+                    length++;
+                    current = current->next;
+                }
+    
+                return length;
             }
         }
-        return NULL; 
-    }
-
-    int CycleLength(ListNode *head)
-    {
-        ListNode *meetingPoint = head->detectCycle(head);
-
-        if (!meetingPoint) {
-            return 0;
-        }
-
-        int length = 1;
-
-        ListNode* current = meetingPoint->next;
-
-        while (current != meetingPoint) {
-            length ++;
-            current = current->next;
-        }
-
-        return length;
+    
+        return 0;  
     }
 };
